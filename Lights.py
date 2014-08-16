929548f4dfd51913a21b82fa5ed15ddf0993584c
Mike Boehm <mike@boehm.com.au>
2014-08-16 14:23:55 +0100

    Initial simplified refactor

--- Lights.py
+++ Lights.py
@@ -5,45 +5,95 @@ from threading import Thread
 
 class Lights(object):
 	def __init__(self):
-		self.queue = {}
-	
-	def add_to_queue(self, from_time, duration, end_colour, start_colour = False):
-		fade_event = {'from_time': from_time, 'duration': duration, 'end_colour' : end_colour}
-		self.queue[] = fade_event
-	
-	def fade_loop(self):
-		fade_event = self.queue.pop()
-		if fade_event:
-			self.fade(fade_event)
+		self.current_colour = {'red': 0, 'green': 0, 'blue': 0}
 		
-	# Execute light transition
-	def fade(self, fade_event):
-		from_time = fade_event['from_time']
-		duration = fade_event['duration']
-		end_colour = fade_event['end_colour']
-		# Calculate time till end
-		# Now + duration = end_time
-		end_time = from_time + duration
+	def set_fade(self, fade):
+		# Do fade calcs
+		# Set fade calcs
+		# Start fade_loop
+		pass
+		
+	# Set threaded fade
+	def set_fade(self, fade):
+		duration = fade['duration']
+		end_colour = fade['end_colour']
+			
+		self.fade_end_time = datetime.datetime.now() + duration
+		self.fade_end_colour = end_colour
+		# Get current light colour
+		current_colour = self.get_lights()
 
+		# Get colours differences
+		self.fade_diffs_dict = self.fade_diffs(current_colour, end_colour)
 
 		# Convert seconds into microsecnds
 		total_duration = duration.seconds * 1000000
-		total_duration = float(total_duration)
-
-		print '=' * 10 + ' Start fade loop ' + '=' * 10
-		start_time = time.time()
-		while datetime.datetime.now() <= end_time:
+		self.fade_total_duration = float(total_duration)
+		
+		# Start fade loop
+		fade_loop = Thread(target=self.fade_loop)
+		fade_loop.start()
+		
+		
+	def fade_loop(self):
+		print 'Fade loop'
+		# While now is <= fade end
+			# Fade
+		while datetime.datetime.now() <= self.fade_end_time:
 			# time till end = end_time - now
-			diff = end_time - datetime.datetime.now()
+			diff = self.fade_end_time - datetime.datetime.now()
 			remaining = (diff.seconds * 1000000) + diff.microseconds
-			percent_remaining = round((remaining/total_duration) * 100,2)
-			
-			print percent_remaining
+			percent_remaining = round((remaining/self.fade_total_duration) * 100,2)
 
+			colour = self.fade_colours(self.fade_diffs_dict, percent_remaining)
+			self.set_lights(colour)
+		
 			time.sleep(0.1)
 
-		end_time = time.time()
-		print '=' * 10 + ' Elapsed ' + '=' * 10
-		print end_time - start_time
+		self.set_lights(self.fade_end_colour)
 
-# 		self.set_lights(end_colour)
+	def set_lights(self, colour):
+		red = int(colour['red'])
+		green = int(colour['green'])
+		blue = int(colour['blue'])
+		
+		print 'red: ', red, 'green: ', green, 'blue: ', blue
+		
+		self.colour = {'red': red, 'green': green, 'blue': blue}
+		
+	def get_lights(self):
+		return self.current_colour		
+
+	# Returns a dict of the difference between two colours
+	def fade_diffs(self, start_colour, end_colour):
+		diff_red = self.fade_diff(end_colour['red'], start_colour['red'])
+		diff_green = self.fade_diff(end_colour['green'], start_colour['green'])
+		diff_blue = self.fade_diff(end_colour['blue'], start_colour['blue'])
+
+		return {'red': diff_red, 'green': diff_green, 'blue': diff_blue}
+
+	# Calculate the difference between two tints
+	def fade_diff(self, start_colour, end_colour):
+		diff = (end_colour - start_colour) / 100.00
+		diff_absolute = math.fabs(diff)
+
+		return { 'diff': diff, 'absolute': diff_absolute }
+		
+
+	# Returns colour to be set, based on the colour diffs
+	def fade_colours(self, diffs, percent_remaining):
+		# Calculate values
+		red = self.fade_colour(diffs['red'], percent_remaining)
+		green = self.fade_colour(diffs['green'], percent_remaining)
+		blue = self.fade_colour(diffs['blue'], percent_remaining)
+
+		return {'red': red, 'green': green, 'blue': blue}
+
+	# Child method of fade_colours()
+	def fade_colour(self, colour, percent_remaining):
+		if(colour['diff'] < 0):
+			colour_to_set = (100 - percent_remaining) * colour['absolute']
+		else:
+			colour_to_set = percent_remaining * colour['absolute']
+
+		return colour_to_set
