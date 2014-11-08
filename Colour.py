dd741a737870f4885341681ca7cb1a7948a91d81
Mike Boehm <mike@boehm.com.au>
2014-11-08 22:26:16 +0000

    Colour object

--- Colour.py
+++ Colour.py
@@ -1,5 +1,12 @@
 class Colour(object):
-	hue = 0
-	saturation = 0.0
-	brightness = 0.0
-	kelvin = 2600
+    hue = 0
+    saturation = 0.0
+    brightness = 0.0
+    kelvin = 2600
+
+    def __init__(self, colour=False):
+        if colour:
+            self.hue = colour['hue']
+            self.saturation = colour['saturation']
+            self.brightness = colour['brightness']
+            self.kelvin = colour['kelvin']
