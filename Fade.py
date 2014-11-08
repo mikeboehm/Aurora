8c744b04753d203f687f6ebb6b4eb8b17f2aac39
Mike Boehm <mike@boehm.com.au>
2014-11-08 22:31:31 +0000

    Fade object

---
+++ Fade.py
@@ -0,0 +1,10 @@
+__author__ = 'mike'
+
+
+class Fade(object):
+    colour = ''
+    duration = ''
+
+    def __init__(self, colour, duration):
+        self.colour = colour
+        self.duration = duration
