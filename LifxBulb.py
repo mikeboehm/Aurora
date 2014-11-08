d697079f33dc7a939b08008c27df51086b2b5b85
Mike Boehm <mike@boehm.com.au>
2014-11-08 18:58:29 +0000

    PEP8

--- LifxBulb.py
+++ LifxBulb.py
@@ -1,78 +1,78 @@
 import socket, json, colorsys, urllib2
 class LifxBulb(object):
-	url = ''
-	PORT = 56780
+    url = ''
+    PORT = 56780
 
-	def __init__(self, JsonClient):
-		self.JsonClient = JsonClient
+    def __init__(self, JsonClient):
+        self.JsonClient = JsonClient
 
-		self.hostname = self.get_hostname()
+        self.hostname = self.get_hostname()
 
 
-	def set_colour(self, colour):
-		self.do_request('PUT', '/lights/all/color', colour)
+    def set_colour(self, colour):
+        self.do_request('PUT', '/lights/all/color', colour)
 # 		print 'msg:', response.msg
 # 		print 'code:', response.code
 # 		print 'headers:', response.headers
 # 		print 'url:', response.url
 
-	def do_request(self, request_type, path, body = None):
-		url = "http://%s:%d%s" % (self.hostname, self.PORT, path)
-		opener = urllib2.build_opener(urllib2.HTTPHandler)
+    def do_request(self, request_type, path, body = None):
+        url = "http://%s:%d%s" % (self.hostname, self.PORT, path)
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
 
-		if body:
-			body = json.JSONEncoder().encode(body)
-			request = urllib2.Request(url, data=body)
-		else:
-			request = urllib2.Request(url)
-
-
-		request.add_header('Content-Type', 'application/json')
-		request.get_method = lambda: request_type
-		response = opener.open(request)
+        if body:
+            body = json.JSONEncoder().encode(body)
+            request = urllib2.Request(url, data=body)
+        else:
+            request = urllib2.Request(url)
 
 
-	def get_lights(self):
-		path = '/lights'
-		method = 'GET'
-
-		return self.get_url(path)
-
-	def set_lights_on(self):
-		path = '/lights/all/on'
-		body = {'duration': 0.5}
-
-		return self.do_request('PUT', path, body)
-
-	def set_lights_off(self):
-		path = '/lights/all/off'
-		body = {'duration': 0.5}
-
-		return self.do_request('PUT', path, body)
+        request.add_header('Content-Type', 'application/json')
+        request.get_method = lambda: request_type
+        response = opener.open(request)
 
 
-	def get_url(self, path):
-		url = "http://%s:%d%s" % (self.hostname, self.PORT, path)
-		try:
-			json = self.JsonClient.get(url)
-		except IOError:
-			settings = False
-		return json
+    def get_lights(self):
+        path = '/lights'
+        method = 'GET'
 
-	def put(self, path):
-		url = "http://%s:%d%s" % (self.hostname, self.PORT, path)
+        return self.get_url(path)
 
-		PUT /lights/{selector}/on
+    def set_lights_on(self):
+        path = '/lights/all/on'
+        body = {'duration': 0.5}
 
-	def get_hostname(self):
-		# Generate hostname
-		bonjour_address = socket.gethostname()
-		# If IP address, use as is
-		try:
-			socket.inet_aton(bonjour_address)
-		except socket.error:
-			# Otherwise make sure the address will work
-			if not bonjour_address.endswith('.local'):
-				bonjour_address += '.local'
+        return self.do_request('PUT', path, body)
 
-		return bonjour_address
+    def set_lights_off(self):
+        path = '/lights/all/off'
+        body = {'duration': 0.5}
+
+        return self.do_request('PUT', path, body)
+
+
+    def get_url(self, path):
+        url = "http://%s:%d%s" % (self.hostname, self.PORT, path)
+        try:
+            json = self.JsonClient.get(url)
+        except IOError:
+            settings = False
+        return json
+
+    def put(self, path):
+        url = "http://%s:%d%s" % (self.hostname, self.PORT, path)
+
+        PUT /lights/{selector}/on
+
+    def get_hostname(self):
+        # Generate hostname
+        bonjour_address = socket.gethostname()
+        # If IP address, use as is
+        try:
+            socket.inet_aton(bonjour_address)
+        except socket.error:
+            # Otherwise make sure the address will work
+            if not bonjour_address.endswith('.local'):
+                bonjour_address += '.local'
+
+        return bonjour_address
--- tests/LifxBulbTest.py
+++ tests/LifxBulbTest.py
@@ -12,88 +12,88 @@ import LifxBulb
 from JsonClient import JsonClient
 
 class TestSettings(unittest.TestCase):
-	url = 'http://localhost:56780/lights.json'
+    url = 'http://localhost:56780/lights.json'
 
-	def setUp(self):
-		json_client = JsonClient()
-		self.lifx_bulb = LifxBulb.LifxBulb(json_client)
+    def setUp(self):
+        json_client = JsonClient()
+        self.lifx_bulb = LifxBulb.LifxBulb(json_client)
 
-	def test_get_lights_returns_a_list(self):
-		lights = self.lifx_bulb.get_lights()
-		self.assertIsInstance(lights, list)
+    def test_get_lights_returns_a_list(self):
+        lights = self.lifx_bulb.get_lights()
+        self.assertIsInstance(lights, list)
 
-	def test_sunrise(self):
-		delay = 0.5
-		# Black
-		duration = 0.5
-		black = {
-			"hue": 0,
-			"duration": duration,
-			"saturation" : 1,
-			"brightness" : 0,
-			"kelvin": 2500
-		}
-		self.lifx_bulb.set_colour(black)
-		time.sleep(duration + delay)
+    def test_sunrise(self):
+        delay = 0.5
+        # Black
+        duration = 0.5
+        black = {
+            "hue": 0,
+            "duration": duration,
+            "saturation" : 1,
+            "brightness" : 0,
+            "kelvin": 2500
+        }
+        self.lifx_bulb.set_colour(black)
+        time.sleep(duration + delay)
 
-		# Test black
-		lights = self.lifx_bulb.get_lights()
-		print lights
-		self.assertEquals(black['hue'], lights[0]['color']['hue'])
-		self.assertEquals(black['saturation'], lights[0]['color']['saturation'])
-		self.assertEquals(black['brightness'], lights[0]['color']['brightness'])
-		self.assertEquals(black['kelvin'], lights[0]['color']['kelvin'])
+        # Test black
+        lights = self.lifx_bulb.get_lights()
+        print lights
+        self.assertEquals(black['hue'], lights[0]['color']['hue'])
+        self.assertEquals(black['saturation'], lights[0]['color']['saturation'])
+        self.assertEquals(black['brightness'], lights[0]['color']['brightness'])
+        self.assertEquals(black['kelvin'], lights[0]['color']['kelvin'])
 
-		# Red
-		duration = 0.5
-		red = {
-			"hue": 0,
-			"duration": duration,
-			"saturation" : 1,
-			"brightness" : 1,
-			"kelvin": 2500
-		}
-		print 'set red'
-		self.lifx_bulb.set_colour(red)
-		time.sleep(duration + delay)
+        # Red
+        duration = 0.5
+        red = {
+            "hue": 0,
+            "duration": duration,
+            "saturation" : 1,
+            "brightness" : 1,
+            "kelvin": 2500
+        }
+        print 'set red'
+        self.lifx_bulb.set_colour(red)
+        time.sleep(duration + delay)
 
-		# Test red
-		lights = self.lifx_bulb.get_lights()
-		print lights
-		self.assertEquals(red['hue'], lights[0]['color']['hue'])
-		self.assertEquals(red['saturation'], lights[0]['color']['saturation'])
-		self.assertEquals(red['brightness'], lights[0]['color']['brightness'])
-		self.assertEquals(red['kelvin'], lights[0]['color']['kelvin'])
+        # Test red
+        lights = self.lifx_bulb.get_lights()
+        print lights
+        self.assertEquals(red['hue'], lights[0]['color']['hue'])
+        self.assertEquals(red['saturation'], lights[0]['color']['saturation'])
+        self.assertEquals(red['brightness'], lights[0]['color']['brightness'])
+        self.assertEquals(red['kelvin'], lights[0]['color']['kelvin'])
 
 
-		duration = 0.5
-		white = {
-			"hue": 0,
-			"duration": duration,
-			"saturation" : 0,
-			"brightness" : 1,
-			"kelvin": 2500
-		}
-		print 'set white'
-		self.lifx_bulb.set_colour(white)
-		time.sleep(duration + delay)
-		lights = self.lifx_bulb.get_lights()
-		self.assertEquals(white['hue'], lights[0]['color']['hue'])
-		self.assertEquals(white['saturation'], lights[0]['color']['saturation'])
-		self.assertEquals(white['brightness'], lights[0]['color']['brightness'])
-		self.assertEquals(white['kelvin'], lights[0]['color']['kelvin'])
+        duration = 0.5
+        white = {
+            "hue": 0,
+            "duration": duration,
+            "saturation" : 0,
+            "brightness" : 1,
+            "kelvin": 2500
+        }
+        print 'set white'
+        self.lifx_bulb.set_colour(white)
+        time.sleep(duration + delay)
+        lights = self.lifx_bulb.get_lights()
+        self.assertEquals(white['hue'], lights[0]['color']['hue'])
+        self.assertEquals(white['saturation'], lights[0]['color']['saturation'])
+        self.assertEquals(white['brightness'], lights[0]['color']['brightness'])
+        self.assertEquals(white['kelvin'], lights[0]['color']['kelvin'])
 
-		print 'set black'
-		duration = 0.5
-		black = {
-			"hue": 0,
-			"duration": duration,
-			"saturation" : 1,
-			"brightness" : 0,
-			"kelvin": 2500
-		}
-		time.sleep(duration)
-		self.lifx_bulb.set_colour(black)
+        print 'set black'
+        duration = 0.5
+        black = {
+            "hue": 0,
+            "duration": duration,
+            "saturation" : 1,
+            "brightness" : 0,
+            "kelvin": 2500
+        }
+        time.sleep(duration)
+        self.lifx_bulb.set_colour(black)
 
 # 	def test_bulb_is_off_and_turns_on(self):
 # 		lights = self.lifx_bulb.get_lights()
@@ -107,4 +107,4 @@ class TestSettings(unittest.TestCase):
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
