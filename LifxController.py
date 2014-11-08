4050f355ce603c057d5a08ba0ad8651ce57b1d87
Mike Boehm <mike@boehm.com.au>
2014-11-08 22:31:41 +0000

    LifxController

---
+++ LifxController.py
@@ -0,0 +1,20 @@
+__author__ = 'mike'
+
+
+class LifxController(object):
+    hostname = ''
+
+    def get_lights(self):
+        pass
+
+    def set_colour(self, fade):
+        pass
+
+    def lights_on(self):
+        pass
+
+    def lights_off(self):
+        pass
+
+    def lights_toggle(self):
+        pass
