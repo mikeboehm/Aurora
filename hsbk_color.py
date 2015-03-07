563314b861a12b42a33a030774f05cc5feb1b411
Mike Boehm <mike@boehm.com.au>
2015-03-07 18:36:11 +0000

    HSBK colour model

---
+++ hsbk_color.py
@@ -0,0 +1,12 @@
+class HSBKColor(object):
+    def __init__(self, hue, saturation, brightness, kelvin):
+        self.hue = hue
+        self.saturation = saturation
+        self.brightness = brightness
+        self.kelvin = kelvin
+        # {
+        #     'hue': 0.0,
+        #     'saturation': 0.0,
+        #     'brightness': 1.0,
+        #     'kelvin': 9000
+        # }
