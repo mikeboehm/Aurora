01c92869f21930eb2f640e090295805ae855849c
Mike Boehm <mike@boehm.com.au>
2015-03-08 19:51:35 +0000

    Housekeeping

--- Lights.py
+++ Lights.py
@@ -13,7 +13,8 @@ class Lights(object):
         """
         :type lifx_controller: Lifx.Lifx
         """
-        self.lifx_controller = lifx_controller
+
+        self.light_adapter = lifx_controller
 
         self.logger = logger
         self._log('init()')
@@ -21,30 +22,30 @@ class Lights(object):
     # Turns reading lights off
     def lights_off(self):
         self._log('lights_off()')
-        self.lifx_controller.reading_lights_off()
+        self.light_adapter.reading_lights_off()
 
     # Turns reading lights on
     def lights_on(self):
         self._log('lights_on()')
-        self.lifx_controller.reading_lights_on()
+        self.light_adapter.reading_lights_on()
 
     def toggle_lights(self):
         self._log('toggle_lights()')
         print 'Toggle lights'
-        self.lifx_controller.reading_lights_toggle()
+        self.light_adapter.reading_lights_toggle()
 
     def dawn(self, duration):
         self._log('dawn()')
 
-        self.lifx_controller.dawn(duration)
+        self.light_adapter.dawn(duration)
 
     def sunrise(self, duration):
         self._log('sunrise()')
-        self.lifx_controller.sunrise(duration)
+        self.light_adapter.sunrise(duration)
 
     def shutoff(self, duration):
         self._log('shutoff()')
-        self.lifx_controller.shutoff(duration)
+        self.light_adapter.shutoff(duration)
 
     def _log(self, method_name, message=None):
         log_line = str(method_name)
