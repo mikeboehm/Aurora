e548e1959f85f339cf2ca55f5b83cac62362e9a3
Mike Boehm <mike@boehm.com.au>
2015-03-08 23:08:56 +0000

    New test class for Aurora

--- tests/test_aurora.py
+++ tests/test_aurora.py
@@ -1,59 +1,55 @@
-#!/usr/bin/python
-import unittest, os, sys
-sys.path.append(os.path.abspath('..'))
+from unittest import TestCase
+from mock import MagicMock
+from Aurora import Aurora
 
-import Aurora
-import datetime
-import time
-import random
-from threading import Thread
+__author__ = 'mike'
 
-# os.system('clear')
 
-class TestAurora(unittest.TestCase):
-	def setUp(self):
-		self.aurora = Aurora.Aurora()
-	
-	# @todo cleanup GPIO as setUP runs for each test, which throws errors due to the push-button callback
-	def tearDown(self):
-# 		self.aurora.shutdown()	
-		pass
+class TestAurora(TestCase):
+    def setUp(self):
+        lights = MagicMock()
+        settings = MagicMock()
+        gpio_controller = MagicMock()
+        logger = MagicMock()
 
-	def test_get_alarm_for_day_number(self):
-		sunday_alarm = self.aurora.get_alarm_for_day_number(0)
-		monday_alarm = self.aurora.get_alarm_for_day_number(1)
-		tueday_alarm = self.aurora.get_alarm_for_day_number(2)
-		wednesday_alarm = self.aurora.get_alarm_for_day_number(3)
-		thursday_alarm = self.aurora.get_alarm_for_day_number(4)
-		friday_alarm = self.aurora.get_alarm_for_day_number(5)
-		saturday_alarm = self.aurora.get_alarm_for_day_number(6)
+        self.aurora = Aurora(lights, settings, gpio_controller, logger)
 
-		print 'sunday   ', sunday_alarm['sunrise']['end_time']
-		print 'monday   ', monday_alarm['sunrise']['end_time']
-		print 'tueday   ', tueday_alarm['sunrise']['end_time']
-		print 'wednesday', wednesday_alarm['sunrise']['end_time']
-		print 'thursday ', thursday_alarm['sunrise']['end_time']
-		print 'friday   ', friday_alarm['sunrise']['end_time']
-		print 'saturday ', saturday_alarm['sunrise']['end_time']
+    def test_toggle_light_callback(self):
+        self.fail()
 
-	def test_get_today_alarm(self):
-		today_alarm = self.aurora.get_today_alarm()
-		print today_alarm
-		
-# 	def test_next_alarm(self) :
-# 		next_alarm = self.aurora.get_next_alarm()
-# 		print next_alarm
-# 
-# 	def test_seconds_till_alarm(self) :
-# 		seconds_till_alarm = self.aurora.seconds_till_alarm(datetime.datetime.now() + datetime.timedelta(seconds=10))
-#  		self.assertTrue(10 <= seconds_till_alarm <= 10.2)
-# 
-# 		seconds_till_alarm = self.aurora.seconds_till_alarm(datetime.datetime.now() + datetime.timedelta(days=1))
-#  		self.assertTrue(86400 <= seconds_till_alarm <= 86400.2)
-# 	
-# 	def test_set_alarm(self):	
-# 		self.aurora.set_alarm()
-	
+    def test_rabbit_listener(self):
+        self.fail()
 
-if __name__ == '__main__':
-	unittest.main()
+    def test_rabbit_callback(self):
+        self.fail()
+
+    def test_get_settings(self):
+        self.fail()
+
+    def test_set_alarm(self):
+        self.fail()
+
+    def test_trigger_dawn(self):
+        self.fail()
+
+    def test_trigger_sunrise(self):
+        self.fail()
+
+    def test_trigger_autoshutoff(self):
+        self.fail()
+
+    def test_seconds_till_alarm(self):
+        self.fail()
+
+    def test_get_alarm_for_day_number(self):
+        self.fail()
+
+    def test_get_today_alarm(self):
+        self.fail()
+
+    def test_get_next_alarm(self):
+       self.fail()
+
+
+    def test_shutdown(self):
+        self.fail()
