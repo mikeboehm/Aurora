8121b8636139df93d49033df565b8e0ee25f6542
Mike Boehm <mike@boehm.com.au>
2015-03-08 15:32:04 +0000

    Added FakeLogger for testing

---
+++ fake_logger.py
@@ -0,0 +1,10 @@
+class FakeLogger(object):
+
+    def __init__(self, class_name = None):
+        pass
+
+    def log(self, method_name, message=None):
+        pass
+
+    def write(self, log_record=None, class_name=None):
+        pass
